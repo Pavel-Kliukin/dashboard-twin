caeb63e3f2dec12550c03e34e7deb4a909ffec63
Pavel-Kliukin <9213087@gmail.com>
2024-04-25 02:46:26 +0300

    User added to Navbar

--- components/DashboardLayout.jsx
+++ components/DashboardLayout.jsx
@@ -1,17 +1,17 @@
 const DashboardLayout = () => {
   return (
-    <div class="p-3 flex-1 flex flex-col sm:ml-64 bg-green-100">
+    <div class="p-3 flex-1 flex flex-col sm:ml-64 bg-[#F9FAFC]">
       <div className="h-1/3 w-full flex flex-1">
-        <div class="m-3 flex-1 bg-slate-400 rounded-lg">01</div>
-        <div class="m-3 w-60 flex bg-slate-400 rounded-lg">02</div>
+        <div class="m-3 flex-1 bg-white rounded-lg">01</div>
+        <div class="m-3 w-60 flex bg-white rounded-lg">02</div>
       </div>
       <div className="h-1/3 w-full flex">
-        <div class="m-3 w-1/2 bg-slate-400 rounded-lg">03</div>
-        <div class="m-3 flex-1 bg-slate-400 rounded-lg">04</div>
-        <div class="m-3 flex-1 bg-slate-400 rounded-lg">05</div>
+        <div class="m-3 w-1/2 bg-white rounded-lg">03</div>
+        <div class="m-3 flex-1 bg-white rounded-lg">04</div>
+        <div class="m-3 flex-1 bg-white rounded-lg">05</div>
       </div>
       <div className="h-1/3 w-full flex">
-        <div class="m-3 flex-1 bg-slate-400 rounded-lg">06</div>
+        <div class="m-3 flex-1 bg-white rounded-lg">06</div>
       </div>
     </div>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <div className="h-20 sm:ml-64 flex justify-between items-center bg-gray-300">
-      <div className="pl-10 font-poppins font-bold">Dashboard</div>
-      <div className="m-5 ml-0 h-10 w-10 bg-red-300"></div>
+    <div className="h-20 sm:ml-64 flex justify-between items-center bg-white">
+      <div className="pl-10 font-poppins font-semibold text-2xl">Dashboard</div>
+      <div className="mr-10 flex items-center">
+        <Image
+          src="/me.jpg"
+          alt="profile"
+          className="mr-4 rounded-lg"
+          width="50"
+          height="50"
+        />
+        <div>
+          <div className="font-poppins font-medium text-sm">Pavel</div>
+          <div className="font-poppins font-light text-xs">Admin</div>
+        </div>
+
+      </div>
     </div>
   );
 };
