5d1fa51c64d9bad94e260d0b2761a878fc559fbd
Pavel-Kliukin <9213087@gmail.com>
2024-04-25 04:42:32 +0300

    Icons added to Sidebar and messages notification in Navbar done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,33 @@
 import Image from "next/image";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
 
 const Navbar = () => {
   return (
     <div className="h-20 sm:ml-60 flex justify-between items-center bg-white">
       <div className="pl-8 font-poppins font-semibold text-2xl">Dashboard</div>
       <div className="mr-10 flex items-center">
-        <Image
-          src="/me.jpg"
-          alt="profile"
-          className="mr-4 rounded-lg"
-          width="50"
-          height="50"
-        />
-        <div>
-          <div className="font-poppins font-medium text-sm">Pavel</div>
-          <div className="font-poppins font-light text-xs">Admin</div>
+        
+        <div className="mr-4 w-10 h-10 flex relative justify-center items-center rounded-md bg-yellow-50">
+          <div className="w-1 h-1 absolute top-2 right-2 rounded-full bg-red-600"></div>
+          <NotificationsNoneIcon className="text-yellow-400"/>
         </div>
-        <ExpandMoreIcon 
-        className="ml-6"/>
+
+        <div className="flex">
+          <Image
+            src="/me.jpg"
+            alt="profile"
+            className="mr-4 rounded-lg"
+            width="50"
+            height="50"
+          />
+          <div>
+            <div className="font-poppins font-medium text-sm">Pavel</div>
+            <div className="font-poppins font-light text-xs">Admin</div>
+          </div>
+          <ExpandMoreIcon className="ml-6"/>
+        </div>
 
       </div>
     </div>
