b2e2f1b5445339eeea09624631022b3353f208e1
Pavel-Kliukin <9213087@gmail.com>
2024-04-25 05:24:45 +0300

    Language switcher done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,9 +5,29 @@ import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 
 const Navbar = () => {
   return (
-    <div className="h-20 sm:ml-60 flex justify-between items-center bg-white">
-      <div className="pl-8 font-poppins font-semibold text-2xl">Dashboard</div>
+    <div className="h-20 sm:ml-60 flex justify-between items-center font-poppins bg-white">
+      <div className="pl-8 font-semibold text-2xl">Dashboard</div>
       <div className="mr-10 flex items-center">
+
+        <div className="mr-10">
+        <input
+          type="text"
+          placeholder="&#xF002; Search here..."
+          className="block w-[200px] rounded-xl border-0 py-1.5 pl-7 pr-7 max-sm:pr-4 text-gray-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 text-sm leading-8"
+        />
+        </div>
+
+        <div className="mr-8 flex">
+          <Image
+            src="/flag.png"
+            alt="English language"
+            className="mr-2 rounded-xl"
+            width="20"
+            height="20"
+          />
+          <div className="text-sm font-semibold">{`Eng (US)`}</div>
+          <ExpandMoreIcon fontSize="small" className="ml-3 text-gray-400"/>
+        </div>
         
         <div className="mr-4 w-10 h-10 flex relative justify-center items-center rounded-md bg-yellow-50">
           <div className="w-1 h-1 absolute top-2 right-2 rounded-full bg-red-600"></div>
@@ -18,15 +38,15 @@ const Navbar = () => {
           <Image
             src="/me.jpg"
             alt="profile"
-            className="mr-4 rounded-lg"
+            className="mr-4 rounded-xl"
             width="50"
             height="50"
           />
-          <div>
-            <div className="font-poppins font-medium text-sm">Pavel</div>
-            <div className="font-poppins font-light text-xs">Admin</div>
+          <div className="flex flex-col justify-center items-center">
+            <div className="font-medium text-sm">Pavel</div>
+            <div className="font-light text-xs">Admin</div>
           </div>
-          <ExpandMoreIcon className="ml-6"/>
+          <ExpandMoreIcon fontSize="small" className="mt-2 ml-6"/>
         </div>
 
       </div>
